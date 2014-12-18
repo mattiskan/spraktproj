850aeb0949358cfe4b83ee7386ad8e8a9b2c92f9
Mattis Kancans Envall <mattiskan@gmail.com>
2014-12-18 15:59:03 +0100

    fixade edvards JÄVLA indentation

--- tester.py
+++ tester.py
@@ -5,14 +5,14 @@ from nltk.corpus import gutenberg
 
 #test with n sentences from reuters
 def testRelations(n=1500):
-	sents = []
-	f = open('testData.txt', 'rU')
-	for line in f:
-		sents.append(nltk.word_tokenize(line))
-	#sents = [s for s in gutenberg.sents('carroll-alice.txt')[:n]]
-	results = getRelations(sents)
-	print()
-
-	for i, rel in enumerate(results):
-		print rel
+    sents = []
+    f = open('testData.txt', 'rU')
+    for line in f:
+        sents.append(nltk.word_tokenize(line))
+    #sents = [s for s in gutenberg.sents('carroll-alice.txt')[:n]]
+    results = getRelations(sents)
+    print()
+    
+    for i, rel in enumerate(results):
+        print(rel)
 testRelations()
