2d6bf82ccab8f63b1583c955607d0e256a1131b7
Mattis Kancans Envall <mattiskan@gmail.com>
2014-12-19 15:52:50 +0100

    mattis filer

--- bfs.py
+++ bfs.py
@@ -6,10 +6,11 @@ from triple import Triple
 
 def bfs(start, goal, triples):
     result = []
+    visited = {}
     q = collections.deque()
-    q.append(triples[start])
 
-    visited = set()
+    for word in triples[start]:
+        q.append( [word] )
     
     while len(q) > 0:
         path = q.popleft()
@@ -28,6 +29,7 @@ def bfs(start, goal, triples):
             if not neighbour.B() in visited:
                 nextp = path + [neighbour]
                 q.append(nextp)
+        print()
 
     return result
 
@@ -42,23 +44,54 @@ def build_index(triples):
     return index
 
 
-
-def main():
-    triples = relations.parseFile("testData.txt")
-    
-    index = build_index(triples)
-
-    results = bfs('Ben', 'Jonathan', index)
-
+def print_results(results):
+    print()
     for result in results:
         for path in result:
-            print(path, end="")
-            if path != result[-1]: print(" -> ", end="")
+            print(path.A(), "->", path.B(), end="")
+            if path != result[-1]: print("  -->  ", end="")
         print()
+
+    print("done")
+
+
+def main():
+    files = [ "testData.txt", "obama.txt", "budget.txt"]
+
+    index = collections.defaultdict(list)
+    for file in files:
+        triples = relations.parseFile(file)
+        found = build_index(triples)
+        print("file:", file)
+        for k,v in found.items():
+            print(*v, sep="\n")
+            index[k].extend(v)
+
+    print("Obama:")
+    for word in index['Obama']:
+        print(word.B(), sep="\n")
+
+    results = bfs('Obama', 'GAB', index)
+    
+    print_results(results)
+
+
+
+def test():
+
+    index = collections.defaultdict(list)
+    for i in range(3):
+        index2 = collections.defaultdict(list)
+        index2['a'].append('1')
+        index2['a'].append('2')
+
+        for k,v in index2.items():
+            index[k].extend(v)
+
+    print(index['a'])
+
 
 
 main()
-    
-
 
 
--- relations.py
+++ relations.py
@@ -1,30 +1,36 @@
 import nltk, re
 from triple import *
 
-#grammar = "Relation: {<N.*><V.*><NNP>}"
-
-grammar = r"""
-  NP: {<DT>?<JJ>?<NN.*>+}          # Chunk sequences of DT, JJ, NN
-  VB: {<VB.*>} # Chunk verbs and their arguments
-  REL: {<NP><VB><NP>}           # Chunk NP, VP
-  """
-
-cp = nltk.RegexpParser(grammar,loop=2)
-
+grammar = "Relation: {<N.*>+<V.*><DT>?<NNP>+}"
+cp = nltk.RegexpParser(grammar)
 
 def getRelations(sentences):
     results = []
-    tagged_sents = sentences
-    #for s in sentences:
-    #    tagged_sents.append(nltk.pos_tag(s))
+    tagged_sents = [nltk.pos_tag(s) for s in sentences]
+    tagged_sents = filter(lambda s: s != [], tagged_sents)
+
     for sent in tagged_sents:
         tree = cp.parse(sent)
+
         for subtree in tree.subtrees():
-            if subtree.label() == 'REL':
-                A = [subtree.leaves()[0]]
-                R = subtree.leaves()[1:-1]
-                B = [subtree.leaves()[-1]]
-                results.append(Triple(A,R,B,tree))
+            if subtree.label() == 'Relation':
+
+                words = subtree.leaves()
+
+                A = []
+                i=0
+                while words[i][1].startswith('N'):
+                        A.append(words[i])
+                        i += 1
+
+                R = []
+                while not words[i][1].startswith('N'):
+                        R.append(words[i])
+                        i += 1                        
+                
+                B = words[i:]
+                            
+                results.append(Triple(A,R,B))
     return results
 
 def parseFile(fileName, n=1500):
@@ -32,10 +38,8 @@ def parseFile(fileName, n=1500):
     f = open(fileName, 'rU')
     for line in f:
         sents.append(nltk.word_tokenize(line))
+    #sents = [s for s in gutenberg.sents('carroll-alice.txt')[:n]]
     results = getRelations(sents)
     
 
     return results
-
-def getSynset(word):
-    
