8261a26ff44e88e6a7a9b3f4fb629c1c5126ac87
Mattis Kancans Envall <mattiskan@gmail.com>
2014-12-19 10:53:08 +0100

    gjorde om indenteringen igen

--- relations.py
+++ relations.py
@@ -5,14 +5,14 @@ grammar = "Relation: {<N.*><V.*><NNP>}"
 cp = nltk.RegexpParser(grammar)
 
 def getRelations(sentences):
-        results = []
-        tagged_sents = [nltk.pos_tag(s) for s in sentences]
-        for sent in tagged_sents:
-                tree = cp.parse(sent)
-                for subtree in tree.subtrees():
-                        if subtree.label() == 'Relation':
-                                A = [subtree.leaves()[0]]
-                                R = subtree.leaves()[1:-1]
-                                B = [subtree.leaves()[-1]]
-                                results.append(Triple(A,R,B))
-        return results
+    results = []
+    tagged_sents = [nltk.pos_tag(s) for s in sentences]
+    for sent in tagged_sents:
+        tree = cp.parse(sent)
+        for subtree in tree.subtrees():
+            if subtree.label() == 'Relation':
+                A = [subtree.leaves()[0]]
+                R = subtree.leaves()[1:-1]
+                B = [subtree.leaves()[-1]]
+                results.append(Triple(A,R,B))
+    return results
